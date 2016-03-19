a816cae663fd45a39e2e2d216c2669e123145e7e
Yacine Mekesser <ymekesser@hotmail.com>
2016-03-19 18:13:42 +0100

    Grosse Umstellung im IssueAnalyzer. DB-Anbindung und Caching implementiert.

--- gtsoog/issues/IssueAnalyzer.py
+++ gtsoog/issues/IssueAnalyzer.py
@@ -1,63 +1,100 @@
-from gtsoog.issues.interfaces.GitHub import GitHub
 import re
 
-# TODO: make usable lol
-# TODO: implement caching for repeated issues
+from sqlalchemy.orm.exc import NoResultFound
+
+from issues.interfaces import GitHub
+from model import DB
+from model.objects.Issue import Issue
+from model.objects.IssueTracking import IssueTracking, TYPE_GITHUB
+from utils import Log
+
+# TODO: Relationship typ bestimmen für commit <-> issue: Associated, Closes, etc.
+
+__issue_cache = {}
 
 
-def extract_issue_ids(commit_message, search_pattern, extract_pattern=None):
+def analyze_repository(repository_id):
+    reset_issue_cache()
+
+    # get issue tracking object
+    Log.info("Retrieving IssueTracking for Repository with id " + str(repository_id))
+    db_session = DB.create_session()
+    query = db_session.query(IssueTracking).filter(IssueTracking.repository_id == int(repository_id))
+    try:
+        issue_tracking = query.one()
+    except NoResultFound:
+        Log.error("No IssueTracking-Entry found for Repository with id " + str(repository_id))
+        db_session.close()
+        return
+    Log.debug("IssueTracking found. Type: " + str(issue_tracking.type))
+
+    if issue_tracking.type == TYPE_GITHUB:
+        retrieve = GitHub.retrieve
+        extract_pattern = '#[0-9]+'
+        transform = lambda x: x[1:]
+    else:
+        Log.error("No Implementation found for IssueTracking-Type '" + str(issue_tracking.type) + "'")
+        db_session.close()
+        return
+
+    repository = issue_tracking.repository
+    for commit in repository.commits:
+        issue_ids = extract_issue_ids(commit.message, extract_pattern, transform=transform)
+        for issue_id in issue_ids:
+            process_issue(issue_tracking, commit, issue_id, retrieve, db_session)
+
+    Log.info("Issue Analysis completed")
+    db_session.close()
+    reset_issue_cache()
+
+
+def process_issue(issue_tracking, commit, issue_id, retrieve_function, db_session):
+    issue_string = "Issue " + str(issue_id) + " from IssueTracking " + str(issue_tracking.id) + \
+                   " for commit " + commit.id
+    Log.debug("Processing " + issue_string)
+
+    existing_issue = get_existing_issue(db_session, issue_tracking, issue_id)
+    issue = retrieve_function(issue_tracking, commit, issue_id, existing_issue=existing_issue)
+    if not issue:
+        Log.warning(issue_string + " could not be retrieved! Skipping this issue.")
+        return
+    update_issue_cache(issue)
+    Log.debug(issue_string + " was successfully retrieved. Will be persisted now.")
+    issue_tracking.issues.append(issue)
+    commit.issues.append(issue)
+
+    db_session.commit()
+    Log.debug(issue_string + " was successfully processed and persisted.")
+
+
+def extract_issue_ids(commit_message, search_pattern, transform=None):
+    """ Extract issue ids from a commit message
+
+    Args:
+        commit_message (str): The full commit message
+        search_pattern (str): A regular expression to match issue IDs
+        transform (function): Optional. A function to transform the extracted issues, e.g. to make "1234" from "#1234"
+
+    Returns:
+
+    """
     result = re.findall(search_pattern, commit_message)
-    if extract_pattern:
-        result = [re.search(extract_pattern, search_result).group() for search_result in result]
+    if transform:
+        result = [transform(search_result) for search_result in result]
     return result
 
-# These are just tests:
 
-# ==== dotCMS core ====
-dotcms_issue_interface = GitHub('api.github.com', 'dotCMS', 'core')
+def get_existing_issue(db_session, issue_tracking, issue_id):
+    if issue_id in __issue_cache:
+        return __issue_cache[issue_id]
+    query = db_session.query(Issue).filter(Issue.issue_tracking_id == issue_tracking.id, Issue.id == str(issue_id))
+    issue = query.one_or_none()
+    __issue_cache[issue_id] = issue
+    return issue
 
-# Commit: bd867bb7ac6ca098adc95081a11f148aeba98307 (Bug)
-dotcms_commit_message_1 = """Fixes #8424 : Escape file name when downloading."""
-# Commit: afe92be9c05a9018a4819694ebe121044b80c5c1 (Bug)
-dotcms_commit_message_2 = """Fixes #8586 : Fixing casting problem when instantiating the Host class."""
-# Commit: 613594c9e4ff77a9c848d4d71483f1891d3d516e (Bug)
-dotcms_commit_message_3 = """proposed fix #8238"""
-# Commit: bb5449da3be2e245a5d8f6323ff1220db6b63126 (Bug)
-dotcms_commit_message_4 = """fixes #7793 : Unable to edit child folder name """
-# Commit: 292e16e259523cbcc67580d56fd97fa7d63b10ef (No Bug)
-dotcms_commit_message_5 = """#7880 added method """
-# Commit: 0a04222e3557b0289e476b021f67fad89e3d1896 (No Bug)
-dotcms_commit_message_6 = """closes #6584"""
-# Commit: 9567d374492a8f1dc8946b23b7cc887f6deccded (No Issue)
-dotcms_commit_message_7 = """card762: 4 spaces instead tab """
-# Commit: 31270a31569daf77a5e8d90b037f41a129a7137a (No Issue)
-dotcms_commit_message_8 = """Update RulePermissionableUtil.java """
 
-dotcms_commit_messages = [
-    dotcms_commit_message_1,
-    dotcms_commit_message_2,
-    dotcms_commit_message_3,
-    dotcms_commit_message_4,
-    dotcms_commit_message_5,
-    dotcms_commit_message_6,
-    dotcms_commit_message_7,
-    dotcms_commit_message_8
-]
+def update_issue_cache(issue):
+    __issue_cache[issue.id] = issue
 
-search_pattern = '#[0-9]+'
-extract_pattern = '[0-9]+'
-for commit_message in dotcms_commit_messages:
-    issue_numbers = extract_issue_ids(commit_message, search_pattern, extract_pattern)
-    if len(issue_numbers) == 0:
-        print("No issue number found.")
-    else:
-        for issue_number in issue_numbers:
-            dotcms_issue_interface.get_issue(issue_number)
-
-"""
-interface_led = GitHub('github.engineering.zhaw.ch/api/v3', 'mekesyac', 'LED-Cube-Prototyper')
-interface_led.get_issue(4)
-
-interface = GitHub('github.engineering.zhaw.ch/api/v3', 'BA16-ML-Java-Analysis', 'Documents')
-interface.get_issue(10)
-"""
+def reset_issue_cache():
+    __issue_cache = {}
--- gtsoog/issues/interfaces/GitHub.py
+++ gtsoog/issues/interfaces/GitHub.py
@@ -2,91 +2,110 @@ from json.decoder import JSONDecodeError
 
 import requests
 
+from model.objects.Commit import Commit
+from model.objects.Issue import Issue, TYPE_BUG, TYPE_ENHANCEMENT, TYPE_OTHER
+from model.objects.IssueTracking import IssueTracking
 from utils import Log
 
 
-class GitHub(object):
+def retrieve(issue_tracking, commit, issue_nr, existing_issue=None):
+    """
 
-    BUG_LABELS = ('bug', 'defect')
+    Args:
+        issue_tracking (IssueTracking): The issue tracking
+        commit (Commit): The commit
+        issue_nr (int|str): The issue number
+        existing_issue (Issue): Optional. Provide if there exists already an issue in the DB.
+    """
+    issue_nr = str(issue_nr)
+    assert issue_nr.isnumeric()
 
-    def __init__(self, api_url, owner, repo, use_https=True, user=None, password=None, bug_label_names=BUG_LABELS):
-        self.api_url = api_url
-        self.owner = owner
-        self.repo = repo
-        self.use_https = use_https
-        self.user = user
-        self.password = password
-        self.bug_label_names = bug_label_names
+    Log.info("Analyzing Issue #" + issue_nr + " for Commit " + commit.id + \
+             " from issue tracking with id " + str(issue_tracking.id))
 
-    def get_protocol_prefix(self):
-        if self.use_https:
-            return 'https://'
-        return 'http://'
+    issue_url = get_issue_url(issue_tracking.url, issue_nr)
 
-    def get_issue_url(self, number):
-        return '{protocol}{domain}/repos/{owner}/{repo}/issues/{number}'.format(
-            protocol=self.get_protocol_prefix(),
-            domain=self.api_url,
-            owner=self.owner,
-            repo=self.repo,
-            number=number
+    auth = None
+    if issue_tracking.username and issue_tracking.password:
+        auth = (issue_tracking.username, issue_tracking.password)
+    Log.debug("Retrieving Issue Nr. " + issue_nr + " via " + issue_url)
+    response = requests.get(issue_url, auth=auth)
+
+    if not response.status_code == 200:
+        Log.error("Issue #" + issue_nr + " could not be retrieved from " + issue_url + \
+                  ". Status Code: " + str(response.status_code))
+        return None
+
+    try:
+        issue_json = response.json()
+    except JSONDecodeError:
+        Log.error("Issue #" + issue_nr + " retrieved from " + issue_url + " could not be parsed to JSON")
+        return None
+
+    # Check for some fields to determine wether the parsed data seems to be an issue
+    if 'number' not in issue_json or not str(issue_json['number']) == issue_nr or 'labels' not in issue_json:
+        Log.error("Retrieved JSON for Issue #" + issue_nr + " from " + issue_url + \
+                  " doesn't seem to represent an actual issue.")
+        return None
+
+    # Determine type from labels
+    label_names = [label['name'] for label in issue_json['labels']]
+    issue_type = get_issue_type(label_names)
+    Log.debug("Issue #" + issue_nr + " has following labels: " + str(label_names) + ". Resulting type: " + issue_type)
+
+    if existing_issue:
+        existing_issue.title = issue_json['title']
+        existing_issue.type = issue_type
+        return existing_issue
+    else:
+        return Issue(
+            id=issue_nr,
+            title=issue_json['title'],
+            type=issue_type
         )
 
-    def __is_bug_label(self, label_name):
-        """ Returns true if the label seems to indicate a bug
 
-        Args:
-            label_name (str): Name of the label
-        """
-        label_name = label_name.lower()
-        for bug_label_name in self.bug_label_names:
-            if bug_label_name in label_name:
-                return True
-        return False
+def get_issue_url(api_url, issue_nr):
+    """ Returns the url to access an issue resource.
 
-    def get_issue(self, number):
-        url = self.get_issue_url(number)
-        auth = None
-        if self.user and self.password:
-            auth = (self.user, self.password)
+    Args:
+        api_url (str): The url to the git API. E.g. "github.engineering.zhaw.ch/api/v3/repos/mekesyac/LED-Cube-Prototyper"
+        issue_nr (int|str): The issue number
+    """
+    return 'https://{url}/issues/{number}'.format(
+        url=api_url,
+        number=issue_nr
+    )
 
-        Log.log('Retrieving Issue Nr. ' + str(number) + ' via ' + url, Log.LEVEL_DEBUG)
-        response = requests.get(url, auth=auth)
 
-        if not response.status_code == 200:
-            message = 'Issue Nr. {number} could not be retrieved from Repository {repo}. Status Code: {status}'.format(
-                number=number,
-                repo=self.repo,
-                status=response.status_code)
-            Log.log(message, Log.LEVEL_WARNING)
-            return None
+def get_issue_type(label_names):
+    """ Returns the type of an issue depending on it's labels """
+    issue_types = [get_label_type(label_name) for label_name in label_names]
 
-        try:
-            issue = response.json()
-        except JSONDecodeError:
-            message = 'Issue Nr. {number} from Repository {repo} contains invalid JSON'.format(
-                number=number,
-                repo=self.repo)
-            Log.log(message, Log.LEVEL_WARNING)
-            return None
+    # Choose one Issue type. Bug has highest priority
+    if TYPE_BUG in issue_types:
+        return TYPE_BUG
+    elif TYPE_ENHANCEMENT in issue_types:
+        return TYPE_ENHANCEMENT
+    else:
+        return TYPE_OTHER
 
-        if 'number' not in issue or not str(issue['number']) == str(number) or 'labels' not in issue:
-            message = 'Retrieved JSON for Issue Nr. {number} from Repository {repo} doesn\'t ' + \
-                      'seem to represent an actual issue.'.format(
-                          number=number,
-                          repo=self.repo)
-            Log.log(message, Log.LEVEL_WARNING)
-            return None
 
-        label_names = [label['name'] for label in issue['labels']]
-        is_associated_with_bug = any([self.__is_bug_label(label_name) for label_name in label_names])
+label_keywords = {
+    TYPE_BUG: ['bug', 'defect'],
+    TYPE_ENHANCEMENT: ['enhancement', 'feature']
+}
 
-        info_message = 'Retrieved JSON for Issue Nr. {number} from Repository {repo}.'.format(
-            number=number,
-            repo=self.repo
-        )
-        if is_associated_with_bug:
-            info_message += ' Seems to be a bug.'
-        Log.log(info_message, Log.LEVEL_INFO)
 
-        return issue
+def get_label_type(label_name):
+    """ Returns the issue-type which a label is associated with.
+
+    Args:
+        label_name (str): Name of the label
+    """
+    label_name = label_name.lower()
+    for label_type, keywords in label_keywords.items():
+        for keyword in keywords:
+            if keyword in label_name:
+                return label_type
+    return None
